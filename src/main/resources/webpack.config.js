c658c46ef7195f6f31a166314ba7ea554c9ccb6a
Arthur Correa <acorrea@athenahealth.com>
2018-04-22 15:16:35 -0400

    start to convert to react

---
+++ src/main/resources/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractLess = new ExtractTextPlugin({
+	filename: "[name].[contenthash].css",
+	disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+	entry: {
+		AdminApp: './code/react/apps/AdminApp.js',
+		MainApp: './code/react/apps/MainApp.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, './static/script/dist')
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			},
+			{
+				test: /\.less/,
+				loaders: ['style-loader', 'css-loader', 'less-loader']
+			}]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.less']
+	},
+	plugins: [
+		extractLess
+	]
+};
