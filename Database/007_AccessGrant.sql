aced2ef82db1c52c69d897dd2290a8ca24610e38
A370224 <a370224@A353224A-BRL-OEM.local>
2019-04-17 09:44:03 -0400

    update to add in a dev auth 0 tenant git push origin master

--- Database/007_AccessGrant.sql
+++ Database/007_AccessGrant.sql
@@ -7,7 +7,7 @@ CREATE TABLE `RadarAccessGrants`(
 	UNIQUE INDEX `IX_RadarAccessGrants_Id` (`Id` ASC));
 
 	    /****** Object:  ForeignKey [FK_TechnologyAssessment_AssessmentTeam]    Script Date: 03/12/2015 12:16:42 ******/
-ALTER TABLE `RadarAccessGrantee`  ADD  CONSTRAINT `FK_RadarAccessGrants_RadarUser_Owner` FOREIGN KEY(`UserId`)
+ALTER TABLE `RadarAccessGrants`  ADD  CONSTRAINT `FK_RadarAccessGrants_RadarUser_Owner` FOREIGN KEY(`UserId`)
 REFERENCES `RadarUser` (`Id`);
 
 ALTER TABLE `RadarAccessGrants`  ADD  CONSTRAINT `FK_RadarAccessGrants_RadarUser_Grant` FOREIGN KEY(`UserId`)
