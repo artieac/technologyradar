00b82c5b0afaec0b52589bcf12e87678ca354c15
Arthur <correaa@aetna.com>
2019-08-09 13:24:15 -0400

    update to add in team databasescript

--- Database/011_AdminRoleSupport.sql
+++ Database/011_AdminRoleSupport.sql
@@ -1,51 +1,40 @@
 SET SQL_SAFE_UPDATES = 0;
 
-ALTER TABLE `RadarTypes` ADD COLUMN `CreateDate` TIMESTAMP NOT NULL DEFAULT current_timestamp;
+CREATE TABLE `Roles`(
+	`Id` INT NOT NULL AUTO_INCREMENT,
+	`Name` NVARCHAR(100) NOT NULL,
+	PRIMARY KEY (`Id`),
+	UNIQUE INDEX `IX_Role_Id` (`Id` ASC));
 
-ALTER TABLE `RadarTypes` ADD COLUMN `Version` BIGINT NOT NULL DEFAULT 1;
+INSERT INTO Roles (Name) VALUES ("User");
+INSERT INTO Roles (Name) VALUES( "Admin"); 
 
-ALTER TABLE `RadarRings` DROP FOREIGN KEY `FK_RadarRings_RadarTypes`;
-ALTER TABLE `RadarCategories` DROP FOREIGN KEY `FK_RadarCategories_RadarTypes`;
+CREATE TABLE `UserTypes`(
+	`Id` INT NOT NULL AUTO_INCREMENT,
+	`Name` NVARCHAR(100) NOT NULL,
+	PRIMARY KEY (`Id`),
+	UNIQUE INDEX `IX_UserType` (`Id` ASC));
 
-ALTER TABLE `RadarTypes` DROP INDEX IX_RadarType_Id;
+INSERT INTO UserTypes (Name) VALUES ("Free");
+INSERT INTO UserTypes (Name) VALUES( "Trial Subscription"); 
+INSERT INTO UserTypes (Name) VALUES( "Subscribed"); 
+INSERT INTO UserTypes (Name) VALUES( "Team"); 
 
-alter table `RadarTypes` drop primary key, add primary key(Id, Version);
+CREATE TABLE `Teams`(
+	`Id` BIGINT NOT NULL AUTO_INCREMENT,
+	`Name` NVARCHAR(100) NOT NULL,
+	`OwnerId` BIGINT NOT NULL,
+	PRIMARY KEY (`Id`),
+	UNIQUE INDEX `IX_Teams_Id` (`Id` ASC),
+	UNIQUE INDEX `IX_Teams_OwnerId` (`OwnerId` ASC));
 
-ALTER TABLE `RadarTypes` MODIFY COLUMN `Id` NVARCHAR(36) NOT NULL;
+CREATE TABLE `TeamMembers`(
+	`Id` BIGINT NOT NULL AUTO_INCREMENT,
+	`TeamId` BIGINT NOT NULL,
+	`MemberId` BIGINT NOT NULL,
+	PRIMARY KEY (`Id`),
+	UNIQUE INDEX `IX_TeamMember_Id` (`Id` ASC),
+	UNIQUE INDEX `IX_Teams_TeamId` (`TeamId` ASC),
+	UNIQUE INDEX `IX_Teams_MemberId` (`MemberId` ASC));
 
-UPDATE `RadarTypes` SET Id = 'e9b8779a-3452-47b9-ab49-600162eace3c' WHERE Id = '1';
-UPDATE `RadarTypes` SET Id = 'a2330b55-164a-49a0-a883-56d46c34a399' WHERE Id = '2';
 
-CREATE UNIQUE INDEX IX_RadarType_Id_Version ON RadarTypes(Id, Version);
-
-ALTER TABLE `RadarRings` ADD COLUMN `RadarTypeVersion` BIGINT NOT NULL DEFAULT 1;
-ALTER TABLE `RadarRings` MODIFY COLUMN `RadarTypeId` NVARCHAR(36) NOT NULL;
-
-UPDATE `RadarRings` SET RadarTypeId = 'e9b8779a-3452-47b9-ab49-600162eace3c' Where RadarTypeId = '1';
-UPDATE `RadarRings` SET RadarTypeId = 'a2330b55-164a-49a0-a883-56d46c34a399' Where RadarTypeId = '2';
-
-ALTER TABLE `RadarRings`  ADD  CONSTRAINT `FK_RadarRings_RadarTypes2` FOREIGN KEY(`RadarTypeId`, `RadarTypeVersion`)
-REFERENCES `RadarTypes` (`Id`, `Version`);
-
-ALTER TABLE `RadarCategories` ADD COLUMN `RadarTypeVersion` BIGINT NOT NULL DEFAULT 1;
-ALTER TABLE `RadarCategories` MODIFY COLUMN `RadarTypeId` NVARCHAR(36) NOT NULL;
-
-UPDATE `RadarCategories` SET RadarTypeId = 'e9b8779a-3452-47b9-ab49-600162eace3c' Where RadarTypeId = '1';
-UPDATE `RadarCategories` SET RadarTypeId = 'a2330b55-164a-49a0-a883-56d46c34a399' Where RadarTypeId = '2';
-
-ALTER TABLE `RadarCategories`  ADD  CONSTRAINT `FK_RadarCategories_RadarTypes2` FOREIGN KEY(`RadarTypeId`, `RadarTypeVersion`)
-REFERENCES `RadarTypes` (`Id`, `Version`);
-
-ALTER TABLE `RadarUser` ADD COLUMN `UserType` INT NOT NULL DEFAULT 0;
-
-ALTER TABLE `TechnologyAssessments` ADD COLUMN `RadarTypeVersion` BIGINT NOT NULL DEFAULT 1;
-ALTER TABLE `TechnologyAssessments` MODIFY COLUMN `RadarTypeId` NVARCHAR(36)  NOT NULL;
-
-UPDATE `TechnologyAssessments` SET RadarTypeId = 'e9b8779a-3452-47b9-ab49-600162eace3c' Where RadarTypeId = '1';
-UPDATE `TechnologyAssessments` SET RadarTypeId = 'a2330b55-164a-49a0-a883-56d46c34a399' Where RadarTypeId = '2';
-
-ALTER TABLE `AssociatedRadarTypes` ADD COLUMN `RadarTypeVersion` BIGINT NOT NULL DEFAULT 1;
-ALTER TABLE `AssociatedRadarTypes` MODIFY COLUMN `RadarTypeId` NVARCHAR(36)  NOT NULL;
-
-UPDATE `AssociatedRadarTypes` SET RadarTypeId = 'e9b8779a-3452-47b9-ab49-600162eace3c' Where RadarTypeId = '1';
-UPDATE `AssociatedRadarTypes` SET RadarTypeId = 'a2330b55-164a-49a0-a883-56d46c34a399' Where RadarTypeId = '2';
